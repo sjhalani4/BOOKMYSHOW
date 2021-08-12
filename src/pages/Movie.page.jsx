481ad042331f2d7a72985d0247da4939a167e365
sjhalani4 <sahiljhalani4@gmail.com>
2021-08-12 20:33:47 +0530

    TMDB APIMovies page

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,4 +1,9 @@
 import {FaCcVisa, FaCcApplePay} from "react-icons/fa";
+import React, {useContext,useState,useEffect} from "react";
+import { MovieContext } from "../context/movie.context";
+import axios from "axios";
+import { useParams } from "react-router";
+import Slider from "react-slick";
 //component
 import Cast from "../components/Cast/Cast.component";
 import MovieHero from '../components/MovieHero/MovieHero.component';
@@ -8,10 +13,46 @@ import EntertainmentCardSlider from '../components/Entertainment/Entertainmentca
 import PosterSlider from '../components/PosterSlider/PosterSlider.component';
 //config
 import TempPosters from "../config/TempPosters.config";
+
  
 
 
 const Movie = () => {
+  const {id}= useParams();
+
+  const {movie}=useContext(MovieContext);
+  const [cast, setCast]= useState([]);
+  const [similarMovies, setSimilarMovies ]= useState([]);
+  const [recommended, setRecommended]= useState([]);
+  useEffect(()=> {
+    const requestCast = async() => {
+
+      const getCast = await axios.get(`/movie/${id}/credits`);
+      setCast (getCast.data.cast);
+    };
+    requestCast();
+  },[id]);
+
+    useEffect(()=> {
+
+    const requestSimilarMovies = async() =>{
+      const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+      setSimilarMovies(getSimilarMovies.data.results);
+    };
+    requestSimilarMovies();
+  },[id]);
+
+    useEffect(()=> {
+
+    const requestRecommendedMovies = async() =>{
+      const getRecommendedMovies = await axios.get(`/movie/${id}/recommendations`);
+      setRecommended(getRecommendedMovies.data.results);
+    };
+    requestRecommendedMovies();
+
+
+  },[id]);
+
 
     const settings = {
     
@@ -46,15 +87,46 @@ const Movie = () => {
           },
         ],
       };
-      
+    const settingsCast = {
+    
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 3,
+              //infinite: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 2,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      };
       
     return (<>
     <MovieHero/>
     <div className="my-12 container px-4 lg:ml-20 lg:w-2/3">
         <div className="flex flex-col items-start gap-3">
         <h2 className="text-grey-800 font-bold text-2xl"> About the Movies </h2>
-        <p>Bruce Wayne and Diana Prince try to bring the metahumans of Earth together after the death of Clark Kent.
-             Meanwhile, Darkseid sends Steppenwolf to Earth with an army to subjugate humans.
+        <p>{movie.overview}
              </p>
             </div>
             <div className="my-8">
@@ -90,27 +162,30 @@ const Movie = () => {
          <h2 className="text-grey-800 font-bold text-2xl mb-4"> Cast & Crew </h2>
          
 
-             <div className="flex flex-wrap gap-4">
-             <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ben-affleck-292-12-09-2017-05-12-16.jpg"
-             castName="Ben Affleck" role="Batman"/>
-             <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg"
+             
+               <Slider {...settingsCast}>
+               {cast.map((castdata)=>
+               <Cast image={`https://image.tmdb.org/t/p/original${castdata.profile_path}`}
+               castName={castdata.original_name} role={castdata.character}/> )}
+             </Slider>
+             {/* <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg"
              castName="Henry Cavill" role="Superman"/>
              <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/gal-gadot-11088-17-10-2017-11-45-36.jpg"
              castName="Gal Gadot" role="Wonder woman"/>
              <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amy-adams-148-24-03-2017-12-40-00.jpg"
-             castName="Amy Adams" role="Lois Lane"/> 
-              </div> 
+             castName="Amy Adams" role="Lois Lane"/>  */}
+              
                <div className="my-8">
                 <hr />
                 </div>
                 <div className="my-8 ">
-                <PosterSlider config={settings} images={TempPosters} title="You might also like " isDark={false}  />
+                <PosterSlider config={settings} images={similarMovies} title="You might also like " isDark={false}  />
                 </div>
                 <div className="my-8">
                 <hr />
                 </div>
                 <div className="my-8 ">
-                <PosterSlider config={settings} images={TempPosters} title="BMS XCLUSIV " isDark={false}  />
+                <PosterSlider config={settings} images={recommended} title="BMS XCLUSIV " isDark={false}  />
                 </div>
               </div>
     </div>
